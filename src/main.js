e53d20ce9fa442ea35bdd8c49b9c683f82132f8c
Aurélien Hugues <aurelien.hugues.59@gmail.com>
2019-09-24 11:54:26 +0200

    Add vue-material to project

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import Vue from 'vue';
+import { MdButton, MdContent, MdTabs } from 'vue-material/dist/components';
 import App from './App.vue';
 import router from './router';
 import './registerServiceWorker';
+import 'vue-material/dist/vue-material.min.css';
+import 'vue-material/dist/theme/default.css';
 
 Vue.config.productionTip = false;
+Vue.component('md-button', MdButton);
+Vue.use(MdContent);
+Vue.use(MdTabs);
 
 new Vue({
   router,
--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -3,9 +3,10 @@ import HelloWorld from '@/components/HelloWorld.vue';
 
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
-    const msg = 'new message';
+    const msg = 'Test Test';
     const wrapper = shallowMount(HelloWorld, {
       propsData: { msg },
+      stubs: ['md-button'],
     });
     expect(wrapper.text()).toMatch(msg);
   });
