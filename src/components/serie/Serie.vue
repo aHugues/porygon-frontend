008ac5055a9d83821a344ba822de9c84dade7c2e
Aurélien Hugues <aurelien.hugues.59@gmail.com>
2019-10-01 22:26:34 +0200

    Add basic support for series

---
+++ src/components/serie/Serie.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="full-size">
+    <div class="md-layout full-size">
+      <div class="md-layout-item md-size-20 col-20">
+        <span v-if="category.label === null" class="md-caption">
+          undefined category
+        </span>
+        <span v-else>
+          {{ category.label }}
+        </span>
+      </div>
+      <div class="md-layout-item md-size-60 col-60">
+        {{ serie.title }}
+      </div>
+      <div class="md-layout-item md-size-20 col-20">
+        {{ location.location }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Serie',
+  props: {
+    serie: Object,
+    location: Object,
+    category: Object,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.full-size {
+  width: 100%;
+}
+.col-20 {
+  width: 20%;
+}
+.col-60 {
+  width: 60%;
+}
+</style>
--- src/views/Series.vue
+++ src/views/Series.vue
@@ -1,16 +1,71 @@
 <template>
   <div class="home">
     <md-empty-state
+      v-if="series.length == 0 && !showDialog"
       md-icon="tv"
       md-label="Create your first series"
       md-description="Creating series, you'll be able to store and order your series library.">
       <md-button class="md-primary md-raised">Create your first series</md-button>
     </md-empty-state>
+
+     <div v-if="series.length > 0">
+      <md-list>
+        <div v-for="(serie, key) in series" :key="key">
+          <md-list-item @click="onSelect(serie.Serie.id)">
+            <serie
+            :serie="serie.Serie" :category="serie.Category" :location="serie.Location"
+            ></serie>
+          </md-list-item>
+          <md-divider></md-divider>
+        </div>
+      </md-list>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+import Serie from '@/components/serie/Serie.vue';
+// import LocationForm from '@/components/location/LocationForm.vue';
+import config from '../config';
+
 export default {
-  name: 'Series',
+  name: 'SeriesView',
+  created() {
+    this.fetchData();
+  },
+  data() {
+    return {
+      series: [],
+      environment: process.env.NODE_ENV,
+      showDialog: false,
+    };
+  },
+  components: {
+    Serie,
+  },
+  computed: {
+    apiBaseUrl() { return config[this.environment].porygonApiBaseUrl; },
+    authenticationRequired() { return config[this.environment].porygonApiAuthentication; },
+  },
+  methods: {
+    buildHeaders() {
+      const headers = { 'Content-Type': 'application/json' };
+      if (this.authenticationRequired) {
+        headers.Authorization = `Bearer ${localStorage.getItem('vue-token')}`;
+      }
+      return headers;
+    },
+    fetchData() {
+      axios
+        .get(`${this.apiBaseUrl}/series`, {
+          headers: this.buildHeaders(),
+        })
+        .then((response) => { this.series = response.data; });
+    },
+    onSelect(id) {
+      console.log(`Selected serie ${id}`);
+    },
+  },
 };
 </script>
