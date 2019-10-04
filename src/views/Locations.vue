45eb558ad89f8ad9c49c7fcb2084f4a25aa2d20b
Aurélien Hugues <aurelien.hugues@supelec.fr>
2019-10-04 20:11:40 +0200

    Solve location create button not working

--- src/views/Locations.vue
+++ src/views/Locations.vue
@@ -5,7 +5,7 @@
     md-icon="folder"
     md-label="Create your first location"
     md-description="Creating locations, you'll be able to store and order movies and series.">
-    <md-button class="md-primary md-raised">Create your first location</md-button>
+    <md-button @click="newLocation()" class="md-primary md-raised">Create your first location</md-button>
    </md-empty-state>
 
    <div class="edit-form-wrapper" v-if="showDialog">
