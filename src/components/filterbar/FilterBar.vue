b323f43711d2e6b64eafcfff4eb453be050aa138
Aurélien Hugues <aurelien.hugues.59@gmail.com>
2020-02-23 18:24:34 +0100

    Ref #37: Add right drawer for future filter bar

---
+++ src/components/filterbar/FilterBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2 class="md-display-1">Work in progress</h2>
+
+    This sidebar will have filters in the future, but in the meantime is disabled.
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FilterBar',
+
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>>
