0325d1123f8011798d427b7ef20dcf05730f2da0
Aurélien Hugues <aurelien.hugues.59@gmail.com>
2020-03-07 19:31:03 +0100

    Ref #21: Add summary cards to home page

---
+++ src/components/misc/StatCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <md-card>
+    <md-card-header>
+      <div class="md-title">
+        {{ $ml.get('navigation')[resourceName] }}
+      </div>
+    </md-card-header>
+    <md-card-content>
+      <div class="md-display-3 stat-value">
+        {{ resourceValue }}
+      </div>
+    </md-card-content>
+  </md-card>
+</template>
+
+<script>
+
+export default {
+  name: 'ElementStat',
+  props: {
+    resourceName: String,
+    resourceValue: Number,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.stat-value {
+  padding-right: 30px;
+  text-align: right;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,60 @@
 <template>
-  <div class="home">
-    <div class="check-card">
+  <div class="md-layout home">
+    <div class="md-layout-item md-size-40">
       <api-check></api-check>
+    </div>
+    <div class="md-layout-item md-size-60 md-layout">
+      <div class="md-layout-item md-size-25">
+        <stat-card resourceName="movies" :resourceValue="movieCount"></stat-card>
+      </div>
+      <div class="md-layout-item md-size-25">
+        <stat-card resourceName="series" :resourceValue="serieCount"></stat-card>
+      </div>
+      <div class="md-layout-item md-size-25">
+        <stat-card resourceName="locations" :resourceValue="locationCount"></stat-card>
+      </div>
+      <div class="md-layout-item md-size-25">
+        <stat-card resourceName="categories" :resourceValue="categoryCount"></stat-card>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 
+import axios from 'axios';
+import requests from '../utils/requests';
 import ApiCheck from '@/components/misc/ApiCheck.vue';
+import StatCard from '@/components/misc/StatCard.vue';
 
 export default {
   name: 'home',
+  data() {
+    return {
+      movieCount: 0,
+      serieCount: 0,
+      locationCount: 0,
+      categoryCount: 0,
+    };
+  },
+  mounted() {
+    this.getStats();
+  },
+  methods: {
+    getStats() {
+      axios.get(requests.buildUrl('stats'), requests.buildOptions())
+        .then((response) => {
+          this.movieCount = response.data.movie_count;
+          this.serieCount = response.data.serie_count;
+          this.locationCount = response.data.location_count;
+          this.categoryCount = response.data.category_count;
+        })
+        .catch((error) => { console.error(error); });
+    },
+  },
   components: {
     ApiCheck,
+    StatCard,
   },
 };
 </script>
@@ -22,6 +63,10 @@ export default {
 
   .check-card {
     width: 40%;
+  }
+
+  .stat-card-wrapper {
+    width: 60%;
   }
 
 </style>
